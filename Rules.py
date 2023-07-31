a3a81b6a811d1f135c100f136625310dc13c4bdb
dhuyvp <dinhhuy.vp.work@gmail.com>
2023-08-01 00:37:15 +0700

    add score matching skills job-resume

---
+++ Rules.py
@@ -0,0 +1,80 @@
+import ast
+from Resources import DEGREES_IMPORTANCE
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class Rules:
+    def __init__(self, labels, resumes, jobs):
+        self.labels = labels
+        self.resumes = resumes
+        self.jobs = jobs
+        self.degrees_importance = DEGREES_IMPORTANCE
+
+    def modifying_type_resume(self, resumes) :
+        resumes['skills'] = ast.literal_eval(resumes['skills'])
+
+        return resumes
+
+    def modifying_type_job(self, jobs) :
+        jobs['Skills'] = ast.literal_eval(jobs['Skills'])
+
+        return jobs
+    
+    # skills matching
+    @staticmethod
+    def unique_job_skills(job) :
+        unique_job_skills = []
+        for i in job['Skills'] :
+            if i not in unique_job_skills :
+                unique_job_skills.append(i)
+
+        return unique_job_skills
+
+    def semantic_similarity(self, job, resume):
+        model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+        # model = SentenceTransformer('xlm-roberta-large')
+
+        score = 0
+        sen = job + resume
+
+        print('\n\njob before encode: ', job)
+        print('\nresume before encode: ', resume)
+
+        
+        sen_embeddings = model.encode(sen)
+        print('\n\n\nsen:', sen, '\n\n\n')
+        print('\n\n\nsen_embeddings:', sen_embeddings.shape, '\n\n\n')
+        
+        for i in range(len(job)) :
+            if job[i] in resume : 
+                score += 1
+            else :
+                if max(cosine_similarity([sen_embeddings[i]], sen_embeddings[len(job):])[0]) >= 0.4 :
+                    score += max(cosine_similarity([sen_embeddings[i]], sen_embeddings[len(job):])[0])
+
+        score = score / len(job)
+        return round(score, 6)
+
+
+    def skills_semantic_matching(self, resume, job_index, job_skills) :
+        resume['Skills job ' + str(job_index) + ' semantic matching'] = 0
+        
+        resume['Skills job ' + str(job_index) + ' semantic matching'] = self.semantic_similarity(job_skills, resume['skills'])
+
+        return resume
+
+    # calculate matching scores
+    def matching_score(self, resume, job, job_index):
+        print('type matching resume:', type(resume), '\n\n')
+        print('matching resume:', resume, '\n\n')
+
+        job_skills = self.unique_job_skills(job)
+        resume = self.skills_semantic_matching(resume, job_index, job_skills)
+
+        skills_score = 0 \
+            + resume['Skills job ' + str(job_index) + ' semantic matching']
+
+        return skills_score
+    
+
+#############################################################
--- main.py
+++ main.py
@@ -1,27 +1,76 @@
 from JobInfoExtract import JobInfoExtract
+from Rules import Rules
 import json
+import ast
 
-def extraction():
+def extraction(job):
     degrees_patterns_path = 'Resources/degrees.jsonl'
     majors_patterns_path = 'Resources/majors.jsonl'
     skills_patterns_path = 'Resources/skills.jsonl'
-    # Resources/example_job_4287.json
-    with open('Resources/example_job_4287.json', 'r') as f:
-        job = json.load(f)
 
     print(job['requirements'], end = '\n\n')
-
 
     job_extraction = JobInfoExtract(skills_patterns_path, majors_patterns_path, degrees_patterns_path, job)
     job_extraction = job_extraction.extract_entites(job)
 
     return job_extraction
 
+def job_resume_matching(job, resume) :
+
+    pass
+
+def modifying_type_resume(resumes):
+    # for i in range(len(resumes["degrees"])):
+    #     resumes["degrees"][i] = ast.literal_eval(resumes["degrees"][i])
+    # for i in range(len(resumes["skills"])):
+    resumes["skills"]= ast.literal_eval(resumes["skills"])
+    return resumes
+
+
+def modifying_type_job(jobs):
+    # for i in range(len(jobs["Skills"])):
+    jobs["Skills"] = ast.literal_eval(jobs["Skills"])
+    return jobs
+
 
 if __name__ == '__main__':
+    # Resources/example_job_4287.json
+    # with open('Resources/example_job_4287.json', 'r') as f:
+    #     job = json.load(f)
+    
+    # with open('Resources/example_resume_5254.json', 'r') as f:
+    #     resume = json.load(f)
 
-    # extraction()
+    with open('Data/job_description/4288.json', 'r') as f:
+        job = json.load(f)
 
-    ### run
-    print('extract information: ', extraction() )
+    with open('Data/resume/4288.json', 'r') as f:
+        resume = json.load(f)
+    
 
+    with open('Resources/labels.json') as fp:
+        labels = json.load(fp)
+
+    job_extracted = extraction(job)
+    # job_extracted = modifying_type_job(job_extracted)
+    # resume = modifying_type_resume(resume)
+
+    print('----------------------------\n')
+    print(job_extracted)
+    print('----------------------------\n')
+    print(resume)
+    
+
+
+    rule_object = Rules(labels, resume, job_extracted)
+    
+    print('type of resume', type(resume), '\ntype of job:', type(job_extracted))
+    
+    skills_score = rule_object.matching_score(resume, job_extracted, 0)
+    print('skills score:', skills_score)
+
+    # print(job_extracted)
+
+
+
+    pass

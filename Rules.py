60bf7df8279527b610089f7ae0a0b303c365c2d1
dhuyvp <dinhhuy.vp.work@gmail.com>
2023-08-03 02:05:34 +0700

    update degree score v1

--- Rules.py
+++ Rules.py
@@ -24,6 +24,7 @@ class Rules:
 
         return jobs
     
+    ###################################
     # skills matching
     @staticmethod
     def unique_job_skills(job) :
@@ -33,21 +34,22 @@ class Rules:
                 unique_job_skills.append(i)
 
         return unique_job_skills
+    
 
     def semantic_similarity(self, job, resume):
-        model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
-        # model = SentenceTransformer('xlm-roberta-large')
+        model = SentenceTransformer("sentence-transformers/all-roberta-large-v1")
+        # model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 
         score = 0
         sen = job + resume
 
-        print('\n\njob before encode: ', job)
-        print('\nresume before encode: ', resume)
+        # print('\n\njob before encode: ', job)
+        # print('\nresume before encode: ', resume)
 
         
         sen_embeddings = model.encode(sen)
-        print('\n\n\nsen:', sen, '\n\n\n')
-        print('\n\n\nsen_embeddings:', sen_embeddings.shape, '\n\n\n')
+        # print('\n\n\nsen:', sen, '\n\n\n')
+        # print('\n\n\nsen_embeddings:', sen_embeddings.shape, '\n\n\n')
         
         for i in range(len(job)) :
             if job[i] in resume : 
@@ -67,18 +69,109 @@ class Rules:
 
         return resume
 
+    ########################################
+    #degree matching
+    @staticmethod
+    def assign_degree_matching(match_scores) :
+        score = 0
+        if (len(match_scores) != 0) :
+            if (max(match_scores) >= 2) :
+                score = 1
+            elif (max(match_scores) >= 0) and (max(match_scores) < 2) :
+                score = 0.5
+        return score
+    
+    @staticmethod
+    def check_degree_level(self, degree):
+        
+        for level, degrees in self.labels['DEGREE'].items():
+            if degree in degrees:
+                return level
+        return ''
+
+
+    def degree_matching(self, resume, job, job_index) :
+        job_min_degree = self.degrees_importance[job['Minimum degree level']]
+
+        resume['Degree job ' + str(job_index) + ' matching'] = 0
+
+        # # print(self.check_degree_level(self, 'ba'))
+
+        match_scores = []
+        for edu in resume['educations'] :
+            score = self.degrees_importance[self.check_degree_level(self, edu['diploma'])] - job_min_degree
+            match_scores.append(score)
+
+        # # print('job min degree: ',job_min_degree)
+
+        resume['Degree job ' + str(job_index) + ' matching'] = self.assign_degree_matching(match_scores)
+
+        # # print('check degrees score:', self.assign_degree_matching(match_scores))
+
+        return resume
+
+    ##########################################
+    #major matching
+    def get_major_category(self, major):
+        """get a major's category"""
+        categories = self.labels['MAJOR'].keys()
+        for c in categories:
+            if major in self.labels['MAJOR'][c]:
+                return c
+
+    def get_job_acceptable_majors(self, job):
+        """get acceptable job majors"""
+        job_majors = job['Acceptable majors']
+        job_majors_categories = []
+        for i in job_majors:
+            job_majors_categories.append(self.get_major_category(i))
+        return job_majors, job_majors_categories
+
+    def get_major_score(self, resume, job):
+        """calculate major matching score for one resume"""
+        resume_majors = []
+        for edu in resume['educations'] :
+            resume_majors.append(edu['major'])
+
+        job_majors, job_majors_categories = self.get_job_acceptable_majors(job)
+        major_score = 0
+        for r in resume_majors:
+            if r in job_majors:
+                major_score = 1
+                break
+            elif self.get_major_category(r) in job_majors_categories:
+                major_score = 0.5
+        return major_score
+    
+
+    def major_matching(self, resumes, jobs, job_index):
+        """calculate major matching score for all resumes"""
+        resumes['Major job ' + str(job_index) + ' matching'] = 0
+        # for i, row in resumes.iterrows():
+        resumes['Major job ' + str(job_index) + ' matching'] = self.get_major_score(resumes,jobs)
+        return resumes
+
+
+
+    #########################################3
     # calculate matching scores
     def matching_score(self, resume, job, job_index):
-        print('type matching resume:', type(resume), '\n\n')
-        print('matching resume:', resume, '\n\n')
-
+        #matching skills
         job_skills = self.unique_job_skills(job)
         resume = self.skills_semantic_matching(resume, job_index, job_skills)
 
         skills_score = 0 \
             + resume['Skills job ' + str(job_index) + ' semantic matching']
 
-        return skills_score
+        #matching degrees
+        resume = self.degree_matching(resume, job, job_index)
+        degrees_score = resume['Degree job ' + str(job_index) + ' matching']
+
+        #matching majors
+        resume = self.major_matching(resume, job, job_index)
+        majors_score = resume['Major job ' + str(job_index) + ' matching']
+
+        return [skills_score, degrees_score, majors_score]
     
 
 #############################################################
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from JobInfoExtract import JobInfoExtract
 from Rules import Rules
+from dotenv import load_dotenv
 import json
 import ast
 import csv
+import os
 
 def extraction(job):
     degrees_patterns_path = 'Resources/degrees.jsonl'
@@ -61,18 +63,24 @@ def get_score_by_job_resume_id(job_id, resume_id) :
     
     # print('type of resume', type(resume), '\ntype of job:', type(job_extracted))
     
-    skills_score = rule_object.matching_score(resume, job_extracted, 0)
+    [skills_score, degrees_score, majors_score] = rule_object.matching_score(resume, job_extracted, job_id)
     print('skills score:', skills_score)
+    print('degrees score:', degrees_score)
+    print('majors score:', majors_score)
 
-    return skills_score
+
+    return [skills_score, degrees_score, majors_score]
 
 if __name__ == '__main__':
-    
+    load_dotenv()
+
+
     with open('final_results.csv', 'w') as f :
         writer = csv.writer(f)
-        writer.writerow(['job_id', 'resume_id', 'skills_score'])
+        writer.writerow(['job_id', 'resume_id', 'skills_score', 'degrees_score'])
 
-        for id in range(4287, 4600) : 
-            writer.writerow([id, id, get_score_by_job_resume_id(id, id)])
+        for id in range(4287, 4300) :
+            [skills_score, degrees_score, majors_score] = get_score_by_job_resume_id(id, id) 
+            writer.writerow([id, id, skills_score, degrees_score, majors_score])
 
     
